5c6a5657edd0e87cf486a8276c991905b8a25279
mbakli <mohamed_bakli@aun.edu.eg>
2023-04-10 21:23:44 +0200

    Modified the worker operators

--- include/post_processing/post_processing.h
+++ include/post_processing/post_processing.h
@@ -1,13 +1,46 @@
 #ifndef POST_PROCESSING_H
 #define POST_PROCESSING_H
+#include "postgres.h"
+#include "distributed/listutils.h"
+
+typedef struct CollectOperator
+{
+    bool active;
+
+} CollectOperator;
+
+typedef struct MergeOperator
+{
+    bool active;
+
+} MergeOperator;
+
+typedef struct WorkerLevelOperator
+{
+    CollectOperator *collectOperator;
+} WorkerLevelOperator;
+
+typedef struct RemDupOperator
+{
+    bool active;
+} RemDupOperator;
+
+typedef struct CoordinatorLevelOperator
+{
+    MergeOperator *mergeOperator;
+    RemDupOperator * dupRemOperator;
+} CoordinatorLevelOperator;
 
 
 
 typedef struct PostProcessing
 {
-    List *functions; // DistributedFunction;
+    CoordinatorLevelOperator *coordinatorLevelOperator;
+    WorkerLevelOperator  *workerLevelOperator;
+    List *distfuns; // DistributedFunction;
     bool group_by_required;
-
+    Datum *genPrimKey;
 } PostProcessing;
 
+extern PostProcessing *PostProcessingQuery(List *strategies);
 #endif /* POST_PROCESSING_H */
