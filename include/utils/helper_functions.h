030cfff43124e2ba4fa60588e0436c92ec40624d
mbakli <mohamed_bakli@aun.edu.eg>
2023-04-16 23:37:04 +0200

    Added change sentence func

--- include/utils/helper_functions.h
+++ include/utils/helper_functions.h
@@ -2,13 +2,15 @@
 #define HELPER_FUNCTIONS_H
 #include "postgres.h"
 #include <nodes/pg_list.h>
+
 // #include "meos.h"
 // #include "meos_internal.h"
 
 
-extern List * ListSpatiotemporalOperations(Node * equals);
-extern char* replaceWord( char* s,  char* oldW,  char* newW);
-extern char * extract_between(const char *str, const char *p1, const char *p2);
+extern List *ListSpatiotemporalOperations(Node * equals);
+extern char *replaceWord( char* s,  char* oldW,  char* newW);
+extern char *extract_between(const char *str, const char *p1, const char *p2);
+extern char *change_sentence (char *sentence, char *find, char *replace);
 extern char *toLower(char *str);
 extern bool IsDatumEmpty(Datum val);
 #endif /* HELPER_FUNCTIONS_H */
