731f4be181575bbbb7f6cc43568c4ae1d9a19d7d
mbakli <mohamed_bakli@aun.edu.eg>
2023-04-16 23:12:38 +0200

    Added catalog utils

---
+++ include/catalog/pg_dist_spatiotemporal_dist_functions.h
@@ -0,0 +1,17 @@
+#ifndef PG_DIST_SPATIOTEMPORAL_DIST_FUNCTIONS_H
+#define PG_DIST_SPATIOTEMPORAL_DIST_FUNCTIONS_H
+
+/* ----------------
+ *  pg_dist_spatiotemporal_dist_functions: Compiler constants
+ * ----------------
+ */
+
+#define Tbl_Dist_Functions "pg_dist_spatiotemporal_dist_functions"
+#define Natts_pg_dist_spatiotemporal_dist_functions 5
+#define Anum_pg_dist_spatiotemporal_dist_functions_id 1
+#define Anum_pg_dist_spatiotemporal_dist_functions_worker 2
+#define Anum_pg_dist_spatiotemporal_dist_functions_combiner 3
+#define Anum_pg_dist_spatiotemporal_dist_functions_final 4
+#define Anum_pg_dist_spatiotemporal_dist_functions_sexec_id 5
+
+#endif /* PG_DIST_SPATIOTEMPORAL_DIST_FUNCTIONS_H */
